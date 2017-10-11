028920359bad05e97558a9c9d53c702546a77764
Unknown <mingkaichen2009@gmail.com>
2017-10-10 23:27:18 -0700

    fixed dependency and require errors

--- services/s2t.js
+++ services/s2t.js
@@ -1,5 +1,5 @@
 const events = require('events');
-const WordSchema = require('../local_db/_schemas');
+const WordSchema = require('../local_db/_schemas/wordstamp_schema');
 
 module.exports = (id, audiostream) => {
     var eventEmitter = new events.EventEmitter();
--- services/uas.js
+++ services/uas.js
@@ -1,5 +1,5 @@
 const request = require('request-promise');
-const WordSchema = require('../local_db/_schemas');
+const WordSchema = require('../local_db/_schemas/wordstamp_schema');
 const uasURL = process.env.UAS_URL || 'http://127.0.0.1:3124';
 
 // required to diminish load on s2t and train s2t
